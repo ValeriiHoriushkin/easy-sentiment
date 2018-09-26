d2952af0f7dd1cac3e2940f188c61023b1e30568
Valerii <horiushkinvalerii@gmail.com>
2018-09-26 22:18:50 +0300

    Sentence split

--- src/sentiwordnet3/SentiWordNet.java
+++ src/sentiwordnet3/SentiWordNet.java
@@ -1,4 +1,4 @@
-//    Copyright 2013 Petter Törnberg
+package sentiwordnet3;//    Copyright 2013 Petter Törnberg
 //
 //    This demo code has been kindly provided by Petter Törnberg <pettert@chalmers.se>
 //    for the SentiWordNet website.
@@ -22,85 +22,46 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class SentiWordNetDemoCode {
+public class SentiWordNet {
 
     private Map<String, Double> dictionary;
 
-    public SentiWordNetDemoCode(String pathToSWN) throws IOException {
-        // This is our main dictionary representation
+    private SentiWordNet(String pathToSWN) throws IOException {
         dictionary = new HashMap<String, Double>();
-
-        // From String to list of doubles.
         HashMap<String, HashMap<Integer, Double>> tempDictionary = new HashMap<String, HashMap<Integer, Double>>();
-
         BufferedReader csv = null;
         try {
             csv = new BufferedReader(new FileReader(pathToSWN));
             int lineNumber = 0;
-
             String line;
             while ((line = csv.readLine()) != null) {
                 lineNumber++;
-
-                // If it's a comment, skip this line.
                 if (!line.trim().startsWith("#")) {
-                    // We use tab separation
                     String[] data = line.split("\t");
                     String wordTypeMarker = data[0];
-
-                    // Example line:
-                    // POS ID PosS NegS SynsetTerm#sensenumber Desc
-                    // a 00009618 0.5 0.25 spartan#4 austere#3 ascetical#2
-                    // ascetic#2 practicing great self-denial;...etc
-
-                    // Is it a valid line? Otherwise, through exception.
                     if (data.length != 6) {
                         throw new IllegalArgumentException(
                                 "Incorrect tabulation format in file, line: "
                                         + lineNumber);
                     }
-
-                    // Calculate synset score as score = PosS - NegS
-                    Double synsetScore = Double.parseDouble(data[2])
-                            - Double.parseDouble(data[3]);
-
-                    // Get all Synset terms
+                    Double synsetScore = Double.parseDouble(data[2])-Double.parseDouble(data[3]);
                     String[] synTermsSplit = data[4].split(" ");
-
-                    // Go through all terms of current synset.
                     for (String synTermSplit : synTermsSplit) {
-                        // Get synterm and synterm rank
                         String[] synTermAndRank = synTermSplit.split("#");
-                        String synTerm = synTermAndRank[0] + "#"
-                                + wordTypeMarker;
-
+                        String synTerm = synTermAndRank[0] + "#" + wordTypeMarker;
                         int synTermRank = Integer.parseInt(synTermAndRank[1]);
-                        // What we get here is a map of the type:
-                        // term -> {score of synset#1, score of synset#2...}
-
-                        // Add map to term if it doesn't have one
                         if (!tempDictionary.containsKey(synTerm)) {
                             tempDictionary.put(synTerm,
                                     new HashMap<Integer, Double>());
                         }
-
-                        // Add synset link to synterm
                         tempDictionary.get(synTerm).put(synTermRank,
                                 synsetScore);
                     }
                 }
             }
-
-            // Go through all the terms.
-            for (Map.Entry<String, HashMap<Integer, Double>> entry : tempDictionary
-                    .entrySet()) {
+            for (Map.Entry<String, HashMap<Integer, Double>> entry : tempDictionary.entrySet()) {
                 String word = entry.getKey();
                 Map<Integer, Double> synSetScoreMap = entry.getValue();
-
-                // Calculate weighted average. Weigh the synsets according to
-                // their rank.
-                // Score= 1/2*first + 1/3*second + 1/4*third ..... etc.
-                // Sum = 1/1 + 1/2 + 1/3 ...
                 double score = 0.0;
                 double sum = 0.0;
                 for (Map.Entry<Integer, Double> setScore : synSetScoreMap
@@ -109,7 +70,6 @@ public class SentiWordNetDemoCode {
                     sum += 1.0 / (double) setScore.getKey();
                 }
                 score /= sum;
-
                 dictionary.put(word, score);
             }
         } catch (Exception e) {
@@ -121,20 +81,35 @@ public class SentiWordNetDemoCode {
         }
     }
 
-    public double extract(String word, String pos) {
+    private double extract(String word, String pos) {
         return dictionary.get(word + "#" + pos);
+    }
+
+    //Aaaaaa bbbb ccccc ddddd .... zzzzz.(!,?)
+    private double sentimentOfSentence(String sentenceString){
+        String[] wordsInSentence = sentenceString.split(" ");
+        String lastWord = wordsInSentence[wordsInSentence.length-1];
+        String typeSentence = lastWord.substring(lastWord.length()-1);
+        wordsInSentence[wordsInSentence.length-1] = lastWord.substring(0,lastWord.length()-1);
+        for (String s : wordsInSentence){
+            System.out.println(s);
+        }
+        System.out.println(typeSentence);
+        return 0.0;
     }
 
     public static void main(String [] args) throws IOException {
         if(args.length<1) {
-            System.err.println("Usage: java SentiWordNetDemoCode <pathToSentiWordNetFile>");
+            System.err.println("Usage: java sentiwordnet3.SentiWordNet <pathToSentiWordNetFile>");
             return;
         }
 
         String pathToSWN = args[0];
-        SentiWordNetDemoCode sentiwordnet = new SentiWordNetDemoCode(pathToSWN);
+        SentiWordNet sentiwordnet = new SentiWordNet(pathToSWN);
+        sentiwordnet.sentimentOfSentence("Today very good day!");
 
-        System.out.println("run#n "+sentiwordnet.extract("run", "n"));
+
+        System.out.println("today "+sentiwordnet.extract("today", ""));
         System.out.println("bad#a "+sentiwordnet.extract("bad", "a"));
         System.out.println("blue#a "+sentiwordnet.extract("blue", "a"));
         System.out.println("blue#n "+sentiwordnet.extract("blue", "n"));
